aa3ee9da78791db5886fc314e5e8182b999c0d33
Wendell <wendellfranco99@gmail.com>
2021-10-04 10:06:15 -0300

    [Feature Wendell - Task: Add PutMapping on TemaController]

--- src/main/java/com/generation/energym/controller/TemaController.java
+++ src/main/java/com/generation/energym/controller/TemaController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +51,12 @@ public class TemaController {
 	@PostMapping
 	public ResponseEntity<TemaModel> Post(@Valid @RequestBody TemaModel tema) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
+	}
+	
+	//Put
+	@PutMapping("/{id}")
+	public ResponseEntity<TemaModel> put(@RequestBody TemaModel tema) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(tema));
 	}
 	
 	//Delete using id
