f27dd18986a02a3e35f1690f129d9f068ef624da
Wendell <wendellfranco99@gmail.com>
2021-10-04 09:56:13 -0300

    [Feature Wendell - Task: Changed NotBlank to NotNull on float economia on TemaModel]

--- src/main/java/com/generation/energym/model/TemaModel.java
+++ src/main/java/com/generation/energym/model/TemaModel.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
@@ -16,13 +17,13 @@ public class TemaModel {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
-	@NotBlank
+	@NotBlank //use Not Blank for string
 	@Size(min = 5, max = 100)
 	private String equipamento;
 
 	private Boolean ativo;
 
-	@NotBlank
+	@NotNull //use Not Null for numbers
 	private Float economia;
 
 	public long getId() {
