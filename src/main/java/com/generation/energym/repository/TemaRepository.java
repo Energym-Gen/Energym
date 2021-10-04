36bedd4c8c1ee811d4b31d3f84394097328b075b
vitorvnc <vitorvnc.f@gmail.com>
2021-10-04 09:19:32 -0300

    Update TemaRepository
    Ajustando o Tema para pegar da Model.

--- src/main/java/com/generation/energym/repository/TemaRepository.java
+++ src/main/java/com/generation/energym/repository/TemaRepository.java
@@ -3,7 +3,9 @@ package com.generation.energym.repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TemaRepository extends JpaRepository<Tema, Long> {
+import com.generation.energym.model.TemaModel;
+
+public interface TemaRepository extends JpaRepository<TemaModel, Long> {
 	
-	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+	public List<TemaModel> findAllByDescricaoContainingIgnoreCase(String descricao);
 }
