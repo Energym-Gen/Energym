177c08892a90602b9657e941d80882b7b1cd3220
vitorvnc <vitorvnc.f@gmail.com>
2021-10-05 18:03:37 -0300

    [Feature Vitor- Task: Implemented]
    Implementado Controlador da classe de  Postagem.

---
+++ src/main/java/com/generation/energym/controller/PostagemController.java
@@ -0,0 +1,58 @@
+package com.generation.energym.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.generation.energym.model.PostagemModel;
+import com.generation.energym.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin("*")
+
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<PostagemModel>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/titulo/{titulo}")
+	public  ResponseEntity<List<PostagemRepository>> GetByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+		
+	}
+	
+	@PostMapping
+	public ResponseEntity<PostagemModel> post (@RequestBody PostagemModel postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity<PostagemModel> put (@RequestBody PostagemModel postagem){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+	   repository.deleteById(id);
+	   
+	}
+	
+	
+
+}
