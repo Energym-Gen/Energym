1a93b167c4dbf95a7643657c35bfb67a5a754163
vitorvnc <vitorvnc.f@gmail.com>
2021-10-13 11:57:13 -0300

    Update BasicSecurityConfig.java
    Corrigindo pacote inserido.

--- src/main/java/com/generation/energym/security/BasicSecurityConfig.java
+++ src/main/java/com/generation/energym/security/BasicSecurityConfig.java
@@ -1,4 +1,4 @@
-package org.generation.energym.security;
+package com.generation.energym.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
