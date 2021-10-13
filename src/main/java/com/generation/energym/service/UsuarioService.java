cc411c2e16d9834abe489b1430b0b1880d300473
gabriela <gabriela17teixeira@gmail.com>
2021-10-13 10:51:50 -0300

    [Feature Gabriela - Implemented UsuarioService]

--- src/main/java/com/generation/energym/service/UsuarioService.java
+++ src/main/java/com/generation/energym/service/UsuarioService.java
@@ -1,5 +1,51 @@
 package com.generation.energym.service;
 
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.generation.energym.model.UsuarioLogin;
+import com.generation.energym.model.UsuarioModel;
+import com.generation.energym.repository.UsuarioRepository;
+
+@Service
 public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository repository;
+
+	public UsuarioModel CadastrarUsuario(UsuarioModel usuario) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		String senhaEncoder = encoder.encode(usuario.getSenha());
+		usuario.setSenha(senhaEncoder);
+
+		return repository.save(usuario);
+	}
+
+	public Optional<UsuarioLogin> logar(Optional<UsuarioLogin> user) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		Optional<UsuarioModel> usuario = repository.findByEmail(user.get().getEmail());
+
+		if (usuario.isPresent()) {
+			if (encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
+
+				String auth = user.get().getEmail() + ":" + user.get().getSenha();
+				byte[] encodeAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic " + new String(encodeAuth);
+
+				user.get().setToken(authHeader);
+				user.get().setNome(usuario.get().getNome());
+
+				return user;
+			}
+		}
+		return null;
+
+	}
 
 }
