a171db76ad90932a8cf87396d615b087808b7561
Matheus <matheus.malmeida7@gmail.com>
2021-10-18 10:09:28 -0300

    [Feature Matheus - Task: Implemented annotations on Usuario of Swagger]

--- src/main/java/com/generation/energym/model/UsuarioModel.java
+++ src/main/java/com/generation/energym/model/UsuarioModel.java
@@ -16,13 +16,19 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.annotations.ApiModelProperty;
+
 @Entity
 @Table(name = "tb_usuario")
 public class UsuarioModel {
 
 	private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long idUsuario;
 	private @NotBlank String nome;
-	private @Email String email;
+	
+	@ApiModelProperty(example = "email@email.com.br")
+	@NotBlank(message = "O atributo Email é Obrigatório!")
+	@Email (message = "O atributo deve ser um email válido!")
+	private String email;
 	private @NotBlank @Size(min = 5 , max = 100 ) String senha;
 	
 	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE )
