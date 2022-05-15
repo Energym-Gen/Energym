fac85f249272d124ec8a7e084d1ffa8f5d683873
gabriela <gabriela17teixeira@gmail.com>
2022-05-15 00:59:58 -0300

    [Feature Gabriela - Task: Criando Projeto]

---
+++ src/main/java/com/uninove/projeto9/Projeto9Application.java
@@ -0,0 +1,13 @@
+package com.uninove.projeto9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/uninove/projeto9/Projeto9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uninove.projeto9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
